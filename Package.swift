f99066a6f8b0c36ff4afc686dced6a06e104926c
Vadim Eisenberg <vadime@il.ibm.com>
2017-03-29 11:15:09 +0300

    IBM-Swift/Kitura#1042 updated dependencies of Bridging and SwifyJSON

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
   name: "Mustache",
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 1, minor: 1),
+    .Package(url: "https://github.com/IBM-Swift/Bridging.git", majorVersion: 1, minor: 7),
     //TODO make this test dependency once issue https://bugs.swift.org/browse/SR-883 is resolved
-      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15, minor: 0)
+      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16, minor: 0)
   ],
   exclude: ["Tests/Carthage", "Tests/vendor", "Tests/Info.plist"]
 )
